f42d982fd6ef935127e899a47edbec55ceccbab6
Marco Túlio <marcot.dns@gmail.com>
2024-10-14 07:02:49 -0300

    Starting footer development

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,10 +6,8 @@ window.addEventListener('scroll', function () {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
     if (scrollTop > lastScrollTop) {
-        // Scroll Down
         navbar.style.top = '-100px';
     } else {
-        // Scroll Up
         navbar.style.top = '0';
     }
     lastScrollTop = scrollTop;
